34ea5134db67abcf211c54cd2b7205d6a2d12766
Hemangi3598 <90692254+Hemangi3598@users.noreply.github.com>
2021-09-16 16:52:10 +0530

    Add files via upload

---
+++ p10.py
@@ -0,0 +1,10 @@
+#wapp to read 2 nos 
+# print min of two
+
+n1= float(input(" enter first num "))
+n2 = float(input(" enter second num "))
+if n1 > n2 :
+	print( n2, " is min ")
+else :
+	print(n1, " is min ")
+print (min(n1,n2), "is min")
